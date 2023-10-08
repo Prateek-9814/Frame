87e343f706a345d5d459a6a1b5e65eeebd8afdf8
Ubuntu <ubuntu@ip-172-31-32-72.ap-south-1.compute.internal>
2023-10-08 18:40:29 +0000

    containerized the present applicatin

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL="postgresql://postgres:123456789@localhost:5432/booking_portal"
+DATABASE_URL="postgresql+psycopg2://postgres:123456789@abc/booking_portal"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
