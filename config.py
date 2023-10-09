d15f406defd6f75586b93cad0545a7c2137227e7
Prateek Kulkarni <130494326+Prateek-9814@users.noreply.github.com>
2023-10-09 22:28:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL="postgresql+psycopg2://postgres:123456789@abc"
+DATABASE_URL="postgresql+psycopg2://postgres:123456789@postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
